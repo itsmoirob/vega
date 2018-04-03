a053af10dc5ba699a3f0a159cf39e120393bc804
Robbie <rob.w.sherman@gmail.com>
2018-04-03 14:51:02 +0100

    Add automapping

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
 using vega.Models;
 using vega.Persistence;
 
@@ -10,16 +11,20 @@ namespace vega.Controllers
   public class MakesController : Controller
   {
     private readonly VegaDbContext context;
-    public MakesController(VegaDbContext context)
+    private readonly AutoMapper.IMapper mapper;
+    public MakesController(VegaDbContext context, AutoMapper.IMapper mapper)
     {
+      this.mapper = mapper;
       this.context = context;
 
     }
 
     [HttpGet("/api/makes")]
-    public async Task<IEnumerable<Make>> GetMakes()
+    public async Task<IEnumerable<MakeResource>> GetMakes()
     {
-        return await context.Makes.Include(m => m.Models).ToListAsync();
+      var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+
+      return mapper.Map<List<Make>, List<MakeResource>>(makes);
     }
   }
 }
